d7ce7c8b0dc83ebc8db05626760546d1d45148f4
Johny <johnybritto.18@gmail.com>
2026-04-18 09:56:35 +0530

    p

--- advanced/ADV_DAY_13.py
+++ advanced/ADV_DAY_13.py
@@ -11,3 +11,17 @@ for i in range(len(A)):
     if A[i] in A[:i] and A[i] not in B:
         B.append(A[i])
 print(B)
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+#another way 
+seen = set()
+duplicates = set()
+
+for item in numbers:
+    if item in seen:
+        duplicates.add(item)
+    else:
+        seen.add(item)
+
+print(duplicates)
